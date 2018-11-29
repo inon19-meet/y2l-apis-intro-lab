060c536439a7fb3140dbfb345eaf4d767d3d20e5
= <=>
2018-11-29 19:03:03 +0200

    l

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
     return render_template('home.html')
 
 ############################
-### Lab Part 1: JSON 
+
 ############################
 
 @app.route('/movie')
@@ -20,7 +20,8 @@ def movies():
                     "image_url": "https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/02/KXC1W2-920x584.jpg"
                     }
                     """
-    return render_template('movie.html', movie={})
+    parsed_json = json.loads(json_string)
+    return render_template('movie.html', movie={},parsed_json = parsed_json)
 
 
 @app.route('/tvshows')
@@ -56,7 +57,6 @@ def tv_shows():
     }]    
     """
     # Write code here to take the `json_string` and return list of movies to the user
-
 
     return render_template('tv_shows.html')
 
